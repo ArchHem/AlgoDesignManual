6d87b646b7ffdd8358569a6ae42e464794365afc
ArchHem <84734676+ArchHem@users.noreply.github.com>
2025-04-29 06:33:08 -0700

    TSP benchmarks

--- Chapters/Chapter1/intro.jl
+++ Chapters/Chapter1/intro.jl
@@ -16,13 +16,12 @@ function TSP_greed_naive_views(x::AbstractMatrix{T}, i = rand(1:size(x)[1])) whe
     
     toq = x
     
-    while size(toq)[1] > 1
+    while size(toq)[2] > 1
         currelem = @view toq[:,i]
-        lmask = trues(size(toq)[2])
-        lmask[i] = false
-        toq = @views toq[:,lmask]
+        
+        toq = @views toq[:,1:end .!= i]
         currmin = typemax(T)
-        @inbounds for j in axes(toq)[2]
+        @inbounds for j in axes(toq,2)
             pot = @views currelem - toq[:,j]
             cdist = dot(pot, pot)
             if cdist < currmin
@@ -37,59 +36,38 @@ function TSP_greed_naive_views(x::AbstractMatrix{T}, i = rand(1:size(x)[1])) whe
     return totdist
 end
 
-function TSP_greed_naive_copies(x::AbstractMatrix{T}, i = rand(1:size(x)[1])) where T
-    #precompute distance matrix 
-    #could do with an abstraction
-    #each row of data is a single entry, i.e. we have dimension (D) number of columns
-
-    #use squared eucleadin distance
-    totdist = zero(T)
-    N, D = size(x)
-    
-    toq = x
-    
-    while size(toq)[1] > 1
-        currelem = @view toq[i,:]
-        lmask = trues(size(toq)[1])
-        lmask[i] = false
-        toq = toq[lmask,:]
-        currmin = typemax(T)
-        @inbounds for j in axes(toq)[1]
-            pot = @views currelem - toq[j,:]
-            cdist = dot(pot, pot)
-            if cdist < currmin
-                currmin = cdist
-                i = j
-            end
-        end
-        totdist += sqrt(currmin)
-    end
-
-
-    return totdist
-end
-
-#comparassion of above methods
-
 using BenchmarkTools, Plots
 
-function compare_greedies(N, D, generator = randn, dtype = Float64)
+
+sizes = [10, 50, 100, 200, 400, 800, 5000, 10000, 50000, 100000, 200000]
+dimensions = 4
+
+function plot_greedy_views_benchmark(sizes::Vector{Int}, D::Int; generator=randn, dtype=Float64)
+    times = zeros(length(sizes))
     
-    t_samples1 = zeros(length(N), length(D))
-    t_samples2 = zeros(length(N), length(D))
-    for (i, n) in enumerate(N)
-        for (j, d) in enumerate(D)
-            println(i," ",j)
-            samples = generator(dtype, n, d)
-            results_views = @benchmark TSP_greed_naive_views($samples,1)
-            results_copies = @benchmark TSP_greed_naive_copies($samples,1)
-            t_samples1[i,j] = mean(results_views.times)
-            t_samples2[i,j] = mean(results_copies.times)
-        end
+    for (i, n) in enumerate(sizes)
+        println("Benchmarking N = $n")
+        samples = generator(dtype, D, n) 
+        result = @benchmark TSP_greed_naive_views($samples, 1)
+        times[i] = mean(result.times) / 1e6  
     end
-    return t_samples1, t_samples2
+
+    # Plotting
+    res = plot(
+        sizes,
+        times,
+        xlabel = "Number of Points (N)",
+        ylabel = "Execution Time (ms)",
+        title = "TSP Greedy Heuristic (Views Only)",
+        marker = :circle,
+        lw = 2,
+        xscale = :log10,
+        yscale = :log10,
+        legend = false,
+        grid = true
+    )
+    return res
 end
 
-sizes = [10, 100, 1000, 10000]
-dimensions = [3]
 
+res1 = plot_greedy_views_benchmark(sizes, 4)
