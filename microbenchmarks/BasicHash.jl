d58904a94b4ebe43bc2c9d4b0adb08f71aa1a0f3
ArchHem <84734676+ArchHem@users.noreply.github.com>
2025-05-31 01:26:15 +0100

    o(1) testing

--- microbenchmarks/BasicHash.jl
+++ microbenchmarks/BasicHash.jl
@@ -5,3 +5,30 @@ using Random
 include("../src/BasicHash.jl")
 using .BasicHash
 
+rng = Xoshiro(1)
+
+#something fukny going on w this benchmark
+#We can see approx O(1) indexing but its midly climbing towards the end.
+function bmark_hashmap()
+    Ns = [2^i for i in 3:14]
+    tmes = zeros(length(Ns))
+    data1 = randn(rng, 4)
+    data2 = randn(rng, 4)
+    pairs = Pair.(data1, data2)
+    hmap = HashMap(pairs...)
+    k = data1[1]
+    getindex(hmap, k)
+
+    for i in eachindex(Ns)
+        data1 = randn(rng, Ns[i])
+        data2 = randn(rng, Ns[i])
+        pairs = Pair.(data1, data2)
+        hmap = HashMap(pairs...)
+        u = @benchmark getindex($hmap, rand($rng, $data1))
+        t = median(u).time
+        tmes[i] = t
+    end
+    return scatter(Ns, tmes, xlabel = "Hash Map initial element count", ylabel = "Key retriaval time (ns)", color = "green", xscale = :log2)
+end
+
+p1 = bmark_hashmap()
