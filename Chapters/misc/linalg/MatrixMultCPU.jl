ba46b75982eaccd5624b557525c59a71d62797a2
ArchHem <84734676+ArchHem@users.noreply.github.com>
2025-06-06 23:58:59 +0200

    more benchmarks for GEMM.

--- Chapters/misc/linalg/MatrixMultCPU.jl
+++ Chapters/misc/linalg/MatrixMultCPU.jl
@@ -208,7 +208,6 @@ function relative_tiled_benchmarks(T = Float64, M_max = 1030, stepsize = 100; ti
     native_res = zeros(T, length(sizes))
     tile_aware_res = zeros(T, length(sizes))
     tile_aware_swap_res = zeros(T, length(sizes))
-    thread_simd_res = zeros(T, length(sizes))
 
     for idx in eachindex(sizes)
         N = sizes[idx]
@@ -247,4 +246,127 @@ function relative_tiled_benchmarks(T = Float64, M_max = 1030, stepsize = 100; ti
     return p
 end
 
-p2 = relative_tiled_benchmarks(Float64, 1030, 100, tile_size = 32)
+#p2 = relative_tiled_benchmarks(Float64, 1030, 100, tile_size = 32)
+
+#We have determined the inner kernel order of kji to be the fastest. Now try two method of top-level parallelism
+#This method assigns 1 thread per chunk; this should be ideal.
+function tile_aware_swap_big_chunks!(c::Matrix{T}, a::Matrix{T}, b::Matrix{T}, tilesize = 16) where T
+    #=
+    actual_tile_size = floor(Int64, cache_bytesize * load_factor / 3)
+    elemsize = sizeof(T)
+    #we each matrix tike to have total tile size of actual_tile_size
+
+    tiledim = floor(Int64,sqrt(actual_tile_size / elemsize))
+    =#
+    @assert size(c, 1) == size(a, 1)
+    @assert size(c, 2) == size(b, 2)
+    @assert size(a, 2) == size(b, 1)
+    fill!(c, zero(T))
+    js = axes(b, 2)
+    jchunks = Iterators.partition(js, length(js) ÷ Threads.nthreads())
+    tasks = map(jchunks) do j
+    @spawn begin
+        for i in partition(axes(a, 1), tilesize)
+            for k in partition(axes(a, 2), tilesize)
+                @inbounds c_view = @views c[i, j]
+                @inbounds a_view = @views a[i, k]
+                @inbounds b_view = @views b[k, j]
+                for lk in axes(a_view, 2)
+                    for lj in axes(b_view, 2)
+                        @simd for li in axes(a_view, 1)
+                            @inbounds c_view[li, lj] = muladd(a_view[li, lk], b_view[lk, lj], c_view[li,lj])
+                        end
+                    end
+                end
+            end
+        end
+    end
+    end
+    fetch.(tasks)
+    return nothing
+end
+
+#We can also try to tile by k in the outermost loop.
+function tile_aware_swap_big_chunks!(c::Matrix{T}, a::Matrix{T}, b::Matrix{T}, tilesize = 16) where T
+    #=
+    actual_tile_size = floor(Int64, cache_bytesize * load_factor / 3)
+    elemsize = sizeof(T)
+    #we each matrix tike to have total tile size of actual_tile_size
+
+    tiledim = floor(Int64,sqrt(actual_tile_size / elemsize))
+    =#
+    @assert size(c, 1) == size(a, 1)
+    @assert size(c, 2) == size(b, 2)
+    @assert size(a, 2) == size(b, 1)
+    fill!(c, zero(T))
+    js = axes(b, 2)
+    jchunks = Iterators.partition(js, length(js) ÷ Threads.nthreads())
+    tasks = map(jchunks) do j
+    @spawn begin
+        for i in partition(axes(a, 1), tilesize)
+            for k in partition(axes(a, 2), tilesize)
+                @inbounds c_view = @views c[i, j]
+                @inbounds a_view = @views a[i, k]
+                @inbounds b_view = @views b[k, j]
+                for lk in axes(a_view, 2)
+                    for lj in axes(b_view, 2)
+                        @simd for li in axes(a_view, 1)
+                            @inbounds c_view[li, lj] = muladd(a_view[li, lk], b_view[lk, lj], c_view[li,lj])
+                        end
+                    end
+                end
+            end
+        end
+    end
+    end
+    fetch.(tasks)
+    return nothing
+end
+
+function relative_tiled_benchmarks_compare_chunks(T = Float64, M_max = 1030, stepsize = 100; tile_size = 32)
+    rng = Xoshiro(3)
+
+    sizes = 30:stepsize:M_max
+    
+    native_res = zeros(T, length(sizes))
+    tile_aware_swap_big_chunks_res = zeros(T, length(sizes))
+    tile_aware_swap_res = zeros(T, length(sizes))
+
+    for idx in eachindex(sizes)
+        N = sizes[idx]
+        println("Benchmarking N = $(N) x $(N) matrices")
+
+        c_native = Matrix{T}(undef, N, N)
+        a_native = randn(rng, T, N, N)
+        b_native = randn(rng, T, N, N)
+
+        c_big_chunks = Matrix{T}(undef, N, N)
+        a_big_chunks = randn(rng, T, N, N)
+        b_big_chunks = randn(rng, T, N, N)
+
+        c_tile_aware_swap = Matrix{T}(undef, N, N)
+        a_tile_aware_swap = randn(rng, T, N, N)
+        b_tile_aware_swap = randn(rng, T, N, N)
+        
+        native_res[idx] = @belapsed mul!($c_native, $a_native, $b_native)
+        
+        tile_aware_swap_big_chunks_res[idx] = @belapsed tile_aware_swap_big_chunks!($c_big_chunks, $a_big_chunks, $b_big_chunks, $tile_size)
+        tile_aware_swap_res[idx] = @belapsed tile_aware_swap!($c_tile_aware_swap, $a_tile_aware_swap, $b_tile_aware_swap, $tile_size)
+    end
+    y_min_plot = 0.0
+    y_max_plot = 0.08
+    grid_interval = 0.005
+    y_ticks = y_min_plot:grid_interval:y_max_plot
+    p = plot(sizes, native_res, label = "Native (BLAS) mul!", color = :black, 
+            linewidth = 2, marker = :circle, yticks = y_ticks, ylim = (y_min_plot, y_max_plot),
+            grid = :y, gridalpha = 0.7, gridlinewidth = 0.5, gridstyle = :dash)
+    plot!(p, sizes, tile_aware_swap_big_chunks_res, label = "Tiled (kjli) - Big Chunks", color = :blue, marker = :square)
+    plot!(p, sizes, tile_aware_swap_res, label = "Tiled (kjli) - @threads j-loop", color = :darkgreen, marker = :x)
+
+    title!("Tile = $tile_size")
+    xlabel!("Matrix Dimension (N)")
+    ylabel!("Execution Time (seconds)")
+    return p
+end
+
+p3 = relative_tiled_benchmarks_compare_chunks()
